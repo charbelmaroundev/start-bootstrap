81ad201db8758c2051a0f23be8c19dc4d3f61671
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-12 23:10:06 +0300

    fix-validation

--- script.js
+++ script.js
@@ -3,50 +3,76 @@ const fullnameEl = document.getElementById('name-input')
 const emailEl = document.getElementById('email-input')
 const phonenumberEl = document.getElementById('phonenumber-input')
 const textareaEl = document.getElementById('textarea-input')
+const wrong1 = document.getElementById('wrong1')
+const wrong2 = document.getElementById('wrong2')
+const wrong3 = document.getElementById('wrong3')
+const wrong4 = document.getElementById('wrong4')
 
 
-let nameValid = false
-let emailValid = false
-let phoneNumberValid = false
-let textareaValid = false
+var nameValid = false
+var emailValid = false
+var phoneNumberValid = false
+var textareaValid = false
 
 
-btnEl.addEventListener('click', () => {
-    nameValidation(nameValid)
-    emailaddressValidation(emailValid)
-    phoneNumberValidalidation(phoneNumberValid)
-    textValidation(textareaValid)
-    // console.log(phonenumberEl.value.slice(0, 4))
+btnEl.addEventListener('click', (e) => {
+    e.preventDefault()
+    nameValidation()
+    emailaddressValidation()
+    phoneNumberValidalidation()
+    textValidation()
+
+
+    if (!nameValid) {
+        console.log(nameValid);
+        wrong1.style.display = "block"
+        wrong1.innerHTML = "Your name is wrong"
+    }
+
+    if (!emailValid) {
+        console.log(emailValid);
+        wrong2.style.display = "block"
+        wrong2.innerHTML = "Your email is wrong"
+    }
+
+
+    if (!phoneNumberValid) {
+        console.log(phoneNumberValid);
+        wrong2.style.display = "block"
+        wrong2.innerHTML = "Your Phone number is wrong"
+    }
+
+
+    if (!textareaValid) {
+        console.log(textareaValid);
+        wrong2.style.display = "block"
+        wrong2.innerHTML = "Your Message is short"
+    }
+
 })
 
 
-
-
-nameValidation = (nameValid) => {
+nameValidation = () => {
     if (fullnameEl.value.length > 4) {
         nameValid = true
-        console.log(nameValid);
     }
 }
 
-emailaddressValidation = (emailValid) => {
+emailaddressValidation = () => {
     if (emailEl.value.includes('@') && emailEl.value.split('@')[0].length > 2 && emailEl.value.split('@')[1].length > 4) {
         emailValid = true
-        console.log(emailValid);
     }
 }
 
-phoneNumberValidalidation = (phoneNumberValid) => {
+phoneNumberValidalidation = () => {
     if (phonenumberEl.value.length < 12 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '3' || phonenumberEl.value.length < 13 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '7') {
         phoneNumberValid = true
-        console.log(phoneNumberValid);
     }
 }
 
-textValidation = (textareaValid) => {
+textValidation = () => {
     if (textareaEl.value.length > 99) {
         textareaValid = true
-        console.log(textareaValid);
     }
 }
 
@@ -54,12 +80,4 @@ textValidation = (textareaValid) => {
 
 
 
-// var str = "Abc: Lorem ipsum sit amet";
-// str = str.substring(str.indexOf(":") + 1);
-// console.log(str);
 
-// const str = 'one two_three four';
-
-// const before_ = str.substring(0, str.indexOf('_'));
-
-// console.log(before_); // 👉️ 'one two'
