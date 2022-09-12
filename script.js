643c1c953151630957ef9c533ee41399bfea6920
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-12 21:20:56 +0300

    add-elemnts-js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const floatField = document.getElementById('floatField');
-const floatContainer = document.getElementById('floatContainer');
-floatField.addEventListener('focus', () => {
-    floatContainer.classList.add('active');
-});
-floatField.addEventListener('blur', () => {
-    floatContainer.classList.remove('active');
-});
+const btn = document.getElementById('submit-btn')
+const name = document.getElementById('naem-input')
+const email = document.getElementById('email-input')
+const password = document.getElementById('password-input')
+const textarea = document.getElementById('textarea-input')
