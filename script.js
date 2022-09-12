3ba67555a84184ca77c6d4e22a41c8ea452f94a4
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-12 23:34:35 +0300

    add-cmt

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////
+// I KNOW MY IS NOT CLEAN BUT NO TIME TO CLEAN IT
+/////////////////////////////////////////////////
 const btnEl = document.getElementById('submit-btn')
 const fullnameEl = document.getElementById('name-input')
 const emailEl = document.getElementById('email-input')
@@ -9,10 +12,43 @@ const wrong3 = document.getElementById('wrong3')
 const wrong4 = document.getElementById('wrong4')
 
 
-var nameValid = false
-var emailValid = false
-var phoneNumberValid = false
-var textareaValid = false
+let nameValid = false
+let emailValid = false
+let phoneNumberValid = false
+let textareaValid = false
+
+
+nameValidation = () => {
+    if (fullnameEl.value.length > 4) {
+        nameValid = true
+    } else {
+        nameValid = false
+    }
+}
+
+emailaddressValidation = () => {
+    if (emailEl.value.includes('@') && emailEl.value.split('@')[0].length > 2 && emailEl.value.split('@')[1].length > 4) {
+        emailValid = true
+    } else {
+        emailValid = false
+    }
+}
+
+phoneNumberValidalidation = () => {
+    if (phonenumberEl.value.length < 12 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '3' || phonenumberEl.value.length < 13 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '7') {
+        phoneNumberValid = true
+    } else {
+        phoneNumberValid = false
+    }
+}
+
+textValidation = () => {
+    if (textareaEl.value.length > 99) {
+        textareaValid = true
+    } else {
+        textareaValid = false
+    }
+}
 
 
 btnEl.addEventListener('click', (e) => {
@@ -22,59 +58,44 @@ btnEl.addEventListener('click', (e) => {
     phoneNumberValidalidation()
     textValidation()
 
-
     if (!nameValid) {
         console.log(nameValid);
         wrong1.style.display = "block"
         wrong1.innerHTML = "Your name is wrong"
+    } else {
+        wrong1.style.display = "none"
+
     }
 
     if (!emailValid) {
         console.log(emailValid);
         wrong2.style.display = "block"
         wrong2.innerHTML = "Your email is wrong"
-    }
+    } else {
+        wrong2.style.display = "none"
 
+    }
 
     if (!phoneNumberValid) {
         console.log(phoneNumberValid);
-        wrong2.style.display = "block"
-        wrong2.innerHTML = "Your Phone number is wrong"
+        wrong3.style.display = "block"
+        wrong3.innerHTML = "Your Phone number is wrong"
+    } else {
+        wrong3.style.display = "none"
     }
-
 
     if (!textareaValid) {
         console.log(textareaValid);
-        wrong2.style.display = "block"
-        wrong2.innerHTML = "Your Message is short"
-    }
+        wrong4.style.display = "block"
+        wrong4.innerHTML = "Your Message is short"
+    } else {
+        wrong4.style.display = "none"
 
+    }
 })
 
 
-nameValidation = () => {
-    if (fullnameEl.value.length > 4) {
-        nameValid = true
-    }
-}
 
-emailaddressValidation = () => {
-    if (emailEl.value.includes('@') && emailEl.value.split('@')[0].length > 2 && emailEl.value.split('@')[1].length > 4) {
-        emailValid = true
-    }
-}
-
-phoneNumberValidalidation = () => {
-    if (phonenumberEl.value.length < 12 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '3' || phonenumberEl.value.length < 13 && phonenumberEl.value.slice(0, 4) == '+961' && phonenumberEl.value.slice(4, 5) == '7') {
-        phoneNumberValid = true
-    }
-}
-
-textValidation = () => {
-    if (textareaEl.value.length > 99) {
-        textareaValid = true
-    }
-}
 
 
 
