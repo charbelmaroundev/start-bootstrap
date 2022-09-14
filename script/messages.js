1e3f5e8e7a112f70e2b3b0ad7102bf29fe6be059
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-14 13:31:03 +0300

    add-fetch-js

--- script/messages.js
+++ script/messages.js
@@ -1,68 +1,15 @@
- const messagesEL = document.querySelector(".messages-list")
+const messagesEL = document.querySelector(".messages-list")
 
 
-let messages = []
 
 fetch('http://localhost/bootstrap/messages.php')
     .then((response) => response.json())
-    .then((data) => data.forEach(element => {
-        messages.push(element.message)
-    }));
-
-
-
-console.log(messagesEL);
-
-console.log(messages);
-
-
-
-
-// // messages.forEach(message => {
-// //     let li = document.createElement('li');
-// //     console.log(messages);
-// // })
-
-// // for (i = 0; i < messages.length; ++i) {
-// //     let li = document.createElement('li');
-// //     li.innerHTML = messages.message[i];
-
-// //     messagesEL.appendChild(li);
-
-// // }
-
-
-
-
-// // let div = document.createElement("Button");
-
-// // div.innerHTML = "Save";
-// // document.querySelector("p").appendChild(div);
-// // const saveEl = document.querySelector("p");
-
-
-// // let data = ["item1", "item2", "item3", "item4"];
-// // let list = document.getElementById("UnList");
-// // for (i = 0; i < data.length; ++i) {
-// //     var li = document.createElement('li');
-// //     li.innerText = data[i];
-// //     list.appendChild(li);
-// // }
-
-
-// // console.log("TEST");
-
-
-// const getData = async () => {
-//     const data = [];
-  
-//     const f1_data = await fetch("https://swapi.dev/api/people/1/")
-//       .then((res) => res.json());
-//     data.push(f1_data.name);
-  
-//     const f2_data = await fetch("https://swapi.dev/api/people/2/")
-//       .then((res) => res.json());
-//     data.push(f2_data.name);
-    
-//     console.log(data);
-//   };
+    .then((data) => {
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i].message);
+            li = document.createElement('p')
+            li.innerHTML = data[i].message
+            messagesEL.appendChild(li)
+        }
+    }
+    );
